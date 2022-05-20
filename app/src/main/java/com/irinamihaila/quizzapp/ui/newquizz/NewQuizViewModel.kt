316c37433b1c5bb55d3e540f8e8e0ff9b573fb16
Marius Stroescu <Marius.Stroescu@ibm.com>
2022-05-20 17:02:44 +0300

    init commit

---
+++ app/src/main/java/com/irinamihaila/quizzapp/ui/newquizz/NewQuizViewModel.kt
@@ -0,0 +1,9 @@
+package com.irinamihaila.quizzapp.ui.newquizz
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.flow
+
+class NewQuizViewModel : ViewModel() {
+
+    fun getQuizQuestions() =  {  }
+}
