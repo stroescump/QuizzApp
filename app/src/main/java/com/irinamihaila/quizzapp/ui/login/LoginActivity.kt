85bf1fd521397d8964934308ec51870f2ee0c5af
Marius Stroescu <Marius.Stroescu@ibm.com>
2022-06-05 16:16:14 +0300

    update to login / register btn

--- app/src/main/java/com/irinamihaila/quizzapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/irinamihaila/quizzapp/ui/login/LoginActivity.kt
@@ -1,11 +1,7 @@
 package com.irinamihaila.quizzapp.ui.login
 
-import android.os.Bundle
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
+import android.graphics.Paint
 import com.irinamihaila.quizzapp.databinding.ActivityLoginBinding
-import com.irinamihaila.quizzapp.models.Question
-import com.irinamihaila.quizzapp.models.Quiz
 import com.irinamihaila.quizzapp.ui.base.BaseActivity
 import com.irinamihaila.quizzapp.ui.dashboard.DashboardActivity
 import com.irinamihaila.quizzapp.ui.registration.RegisterActivity
@@ -16,35 +12,6 @@ import com.irinamihaila.quizzapp.utils.viewBinding
 class LoginActivity : BaseActivity() {
     override val binding by viewBinding(ActivityLoginBinding::inflate)
     private val viewModel by lazy { AuthenticationViewModel(SharedPrefsUtils(this)) }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-//        Firebase.database.reference.child("quizzes").setValue(
-//            listOf(
-//                Quiz(
-//                    "idex", "name1", "GENERAL_KNOWLEDGE", "04.06.2022", true, listOf(
-//                        Question("Ce faceti?", "ans1", "ans2", "ans3", "ans4", "ans3"),
-//                        Question("Ce faceti?", "ans1", "ans2", "ans3", "ans4", "ans3"),
-//                        Question("Ce faceti?", "ans1", "ans2", "ans3", "ans4", "ans3")
-//                    )
-//                ),
-//                Quiz(
-//                    "idex", "name1", "GENERAL_KNOWLEDGE", "04.06.2022", true, listOf(
-//                        Question("Ce faceti?", "ans1", "ans2", "ans3", "ans4", "ans3"),
-//                        Question("Ce faceti?", "ans1", "ans2", "ans3", "ans4", "ans3"),
-//                        Question("Ce faceti?", "ans1", "ans2", "ans3", "ans4", "ans3")
-//                    )
-//                ),
-//                Quiz(
-//                    "idex", "name1", "GENERAL_KNOWLEDGE", "04.06.2022", true, listOf(
-//                        Question("Ce faceti?", "ans1", "ans2", "ans3", "ans4", "ans3"),
-//                        Question("Ce faceti?", "ans1", "ans2", "ans3", "ans4", "ans3"),
-//                        Question("Ce faceti?", "ans1", "ans2", "ans3", "ans4", "ans3")
-//                    )
-//                )
-//            )
-//        )
-    }
 
     override fun setupObservers() {
         viewModel.uiStateLiveData.observe(this) { appResult ->
@@ -79,6 +46,7 @@ class LoginActivity : BaseActivity() {
             }
 
             btnRegister.setOnClickListener {
+                btnRegister.paintFlags = Paint.UNDERLINE_TEXT_FLAG
                 navigateTo(RegisterActivity::class.java)
             }
         }
